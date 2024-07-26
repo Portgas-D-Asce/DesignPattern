94ff9109b8bab4a60b77e4419eaf5a2a2b1ac65e
Portgas·D·Asce <lupengkunmc@163.com>
2024-07-26 13:09:28 +0800

    update singleton template

--- singleton_template.cpp
+++ singleton_template.cpp
@@ -2,23 +2,37 @@
 #include <memory>
 using namespace std;
 
+
 template<typename T>
 class Singleton {
 public:
-    static T& get_instance() {
-        static std::once_flag flag;
-        std::call_once(flag, [&]() {
-            // construct single instance from config file
-            _instance = std::unique_ptr<T>(new T());
+    template<typename... Args>
+    static T& get_instance(Args&&... args) {
+        std::call_once(_init_flag, [&]() {
+            // 这块还有完美转发的必要么，lambda 表达式已经引用了？？？？？？？
+            _instance = std::unique_ptr<T>(new T(std::forward<Args>(args)...));
         });
         return *_instance;
     }
+
+    static void destroy() {
+        static std::once_flag flag;
+        std::call_once(flag, [&]() {
+            _instance.reset(nullptr);
+        });
+    }
 private:
     static std::unique_ptr<T> _instance;
+    // 使用不同的参数构造单例，会导致多个局部静态变量，进而导致多例, 故取消局部静态变量
+    static std::once_flag _init_flag;
 };
 
 template<typename T>
 std::unique_ptr<T> Singleton<T>::_instance = nullptr;
+
+template<typename T>
+std::once_flag Singleton<T>::_init_flag = {};
+
 
 class RealClass {
 public:
